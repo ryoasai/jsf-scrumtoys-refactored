0e29b17ef261a541abfbd1c2a2f29b18851daf16
ryoasai <ryoasai74@gmail.com>
2011-07-23 21:23:36 +0900

    Converted to use Value expression as much as possible instead of <h:outputText>.

---
+++ src/main/java/jsf2/demo/scrum/infra/context/ViewScopedExtention.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jsf2.demo.scrum.infra.context;
+
+/**
+ *
+ * @author Ryo
+ */
+public class ViewScopedExtention {
+    
+}
